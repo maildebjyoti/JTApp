083bc698b25e5d50cd9d484d6f49f98ddb09acd9
Debjyoti Acharjee <maildebjyoti@gmail.com>
2015-12-31 03:26:27 +0530

    Integrating Bcrypt

--- server/routes.js
+++ server/routes.js
@@ -1,14 +1,19 @@
+//TODO
+//Error Handling
+//Data segregation
+
 var jwt = require('jsonwebtoken');
+var bcrypt = require('bcrypt');
 
 var router = require('express').Router();
 var four0four = require('./utils/404')();
 var data = require('./data');
 var db = require('./db');
 
-
 //https://mongodb.github.io/node-mongodb-native/driver-articles/mongoclient.html
 //https://mongodb.github.io/node-mongodb-native/api-generated/mongoclient.html
 //https://mongodb.github.io/node-mongodb-native/api-generated/db.html
+//https://hackhands.com/mongodb-crud-mvc-way-with-passport-authentication/
 
 router.get('/users', getUsers);
 router.get('/user/:id', getUser);
@@ -24,79 +29,35 @@ router.get('/*', four0four.notFoundMiddleware);
 module.exports = router;
 
 //////////////
-var salt = 'abcde';
+var salt = 'there was a c0ld Summer day';
 
-function sign(req, res, next) {
-    var request = req.params.id;
-    console.log(request);
-
-    var user = {
-        name: 'Debjyoti',
-        username: 'deb_xp',
-        password: 'password',
-        admin: true
-    };
-
-    var token = jwt.sign(user, salt, {
+function sign(obj) {
+    return jwt.sign(obj, salt, {
         //algorithm: 'RS512',
         expiresIn: 86400
     });
-
-    //res.send(token);
-    // return the information including token as JSON
-    res.json({
-        success: true,
-        message: 'Enjoy your token!',
-        token: token
-    });
 }
 
-function verify(req, res, next){
-    // check header or url parameters or post parameters for token
-    /*console.log('--1-------------------');
-    console.log(req.body);
-    console.log('--2-------------------');
-    console.log(req.query);
-    console.log('--3-------------------');
-    console.log(req.headers);
-    console.log('----------------------');*/
-    var token = req.body.token || req.query.token || req.headers['x-access-token'] ;
-    //var token = req.params.token;
-    console.log('Token: ' + token);
-    // decode token
+function verify(token){
     if (token) {
-        //console.log('----------------------A');
-        // verifies secret and checks exp
         jwt.verify(token, salt, function (err, decoded) {
                 if (err) {
-                    //console.log('----------------------B');
-                    return res.json({
-                        success: false,
-                        message: 'Failed to authenticate token.'
-                    });
+                    return false;
                 } else {
-                    //console.log('----------------------C');
-                    // if everything is good, save to request for use in other routes
-                    //req.decoded = decoded;
-                    res.send(decoded)
-                    //next();
+                    return true;
                 }
             }
         );
 
-    } else {
-        //console.log('----------------------D');
-        // if there is no token
-        // return an error
-        return res.status(403).send({
-            success: false,
-            message: 'No token provided.'
-        });
-
     }
+    return false;
 }
 
 function getUsers(req, res, next) {
+    // check header or url parameters or post parameters for token
+    //var token = req.body.token || req.query.token || req.headers['x-access-token'] ;
+    //var token = req.params.token;
+
     var collection = db.get().collection('users');
     collection.find().toArray(function (err, docs) {
         //db.close();
@@ -119,42 +80,61 @@ function getUser(req, res, next) {
 }
 
 function doLogin(req, res) {
-    console.log(req.body);
-
-    var user = new User({
+    var user = {
         name: req.body.email,
         username: req.body.email,
         password: req.body.password
-    });
+    };
 
-    res.status(200).send(user);
+    var collection = db.get().collection('users');
+    collection.findOne({
+        'username': user.username
+    }, function (err, data) {
+        if (data !== null) {
+            if (bcrypt.compareSync(user.password, data.password)){
+                delete user.password;
+                delete user._id;
+
+                user.token = sign(user);
+                res.status(200).send(user);
+            } else {
+                console.log('Invalid credentials');
+                res.status(400).send(err);
+            }
+        } else {
+            console.log('Invalid credentials');
+            res.status(400).send(err);
+        }
+    });
 }
 
 function signup(req, res) {
-    console.log(req.body);
-
     var user = {
         name: req.body.name,
         username: req.body.email,
         password: req.body.password
     };
-    
-    //db.users.insert( { name: "rana", username: "rana@test.com", password: "password" } );
 
-    var collection = db.get().collection('users');
-    collection.findOne({'username': user.username}, function(err, data) {
-        if(data === null){
-            collection.insert(user, {safe: true}, function(err, data){
-					console.log("Data inserted - ");
-					console.log(data);
-                    res.status(200).send(data);
-				});
-        }
-        else {
-            console.log('User esists - send error');
-            res.status(400).send(err);
-        }
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            user.password = hash;
+            var collection = db.get().collection('users');
+            collection.findOne({
+                'username': user.username
+            }, function (err, data) {
+                if (data === null) {
+                    collection.insert(user, { safe: true }, function (err, data) {
+                        //Generate Token & send back
+                        delete user.password;
+                        delete user._id;
+                        user.token = sign(user);
+                        res.status(200).send(user);
+                    });
+                } else {
+                    console.log('User esists - send error');
+                    res.status(400).send(err);
+                }
+            });
+        });
     });
-    
-    
 }
