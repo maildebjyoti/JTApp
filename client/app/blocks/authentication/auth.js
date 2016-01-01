f3d7c35843675a8f87bc8557ef2eb692ffdd8bf7
Debjyoti Acharjee <maildebjyoti@gmail.com>
2016-01-02 04:59:35 +0530

    Resolving refresh issue

--- client/app/blocks/authentication/auth.js
+++ client/app/blocks/authentication/auth.js
@@ -57,8 +57,8 @@
         }
 
         function getToken() {
-            //return sessionStorage.getItem('TOKEN');
-            return _token;
+            return sessionStorage.getItem('TOKEN');
+            //return _token;
         }
     }
 }());
