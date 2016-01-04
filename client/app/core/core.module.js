8201bfa6aa6353fc87f523dcecc68f8133dc44a7
Debjyoti Acharjee <maildebjyoti@gmail.com>
2016-01-05 01:27:10 +0530

    FB integration

--- client/app/core/core.module.js
+++ client/app/core/core.module.js
@@ -4,7 +4,7 @@
     angular
         .module('app.core', [
             'ngAnimate', 'ngSanitize',
-            'blocks.exception', 'blocks.logger', 'blocks.router', 'blocks.auth', 'blocks.analytics',
+            'blocks.exception', 'blocks.logger', 'blocks.router', 'blocks.auth', 'blocks.analytics', 'blocks.fb',
             'ui.router', 'ui.bootstrap'
         ]);
 })();
