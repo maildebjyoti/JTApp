4194c095e4b957dacb8c911755ac765353c7fb33
Debjyoti Acharjee <maildebjyoti@gmail.com>
2016-01-04 18:34:19 +0530

    Update controller2.js

--- client/app/mod.planner/controller2.js
+++ client/app/mod.planner/controller2.js
@@ -10,12 +10,27 @@
     function PlannerNewController(logger, analytics) {
         var vm = this;
         vm.title = 'Planner New';
-
+        vm.showModal = false;
+        vm.value = 'abc';
+        
         activate();
 
         function activate() {
             logger.info('Activated Planner New View');
             analytics.tp(vm.title);
         }
+        
+        vm.toggleModal = function() {
+            console.log('toggle modal Before --' + vm.showModal);
+            vm.showModal = !vm.showModal;
+            console.log('toggle modal After --' + vm.showModal);
+        };
+        
+        vm.success = function(){
+            console.log('Value - '+ vm.value);
+            vm.showModal = false;
+        };
+        
     }
 })();
+
